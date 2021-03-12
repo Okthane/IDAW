d63cb1bf72538665547cf7e541c0dda982de0be8
Okthane <80249302+Okthane@users.noreply.github.com>
2021-03-12 14:14:53 +0100

    12/03 à midi

--- TP01/SitePro/template_menu.php
+++ TP01/SitePro/template_menu.php
@@ -4,3 +4,20 @@
         <nav class="item_menu"><a href="hobbies.php">Hobbies</a></nav>
         <nav class="item_menu"><a href="projets.php">Mes projets</a></nav>
     </nav>
+
+<?php
+    function renderMenuToHTML($currentPageId) {
+// un tableau qui d\'efinit la structure du site
+        $mymenu = array(
+// idPage titre
+            'index' => array( 'Accueil' ),
+            'cv' => array( 'Cv' ),
+            'projets' => array('Mes Projets')
+        );
+// ...
+        foreach($mymenu as $pageId => $pageParameters) {
+            echo "...";
+        }
+// ...
+    }
+?>
