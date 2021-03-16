b1c24c99892ddb3f3902ad78266645fe86f36840
Okthane <80249302+Okthane@users.noreply.github.com>
2021-03-16 09:45:21 +0100

    TP3 Questions 1.1, 1.2

--- TP03/login.php
+++ TP03/login.php
@@ -1,4 +1,13 @@
-<form id="login_form" action="connected.php" method="GET">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form id="login_form" action="connected.php" method="POST">
     <table>
         <tr>
         <th>Login :</th>
@@ -14,3 +23,6 @@
         </tr>
     </table>
 </form>
+</body>
+</html>
+
